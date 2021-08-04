06134b387ce5b71b11e8889b26c638a731cb2637
Eliezer Bittencourt <likobittencourt@gmail.com>
2021-08-03 22:45:08 -0300

    Acrescentado o back-end do projeto

---
+++ back-end/README.md
@@ -0,0 +1,9 @@
+# SEQUENCIA PARA CRIAR O PROJETO BACK-END
+## Criar o arquivo package
+### npm init
+
+## Gerencia as requisições, rotas e URLs, entre outra funcionalidades
+### npm install express
+
+## Banco de dados utilizado MySQL
+## Insomnia utilizado para testar requisições
---
+++ back-end/app.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const app = express();
+
+const Produto = require('./models/Produto');
+
+app.use(express.json());
+
+app.get("/list-produto", async (req, res) => {
+    await Produto.findAll({
+        attributes: ['id', 'nome', 'preco_venda', 'quantidade'],
+        order: [['id', 'DESC']]
+    })
+        .then((produtos) => {
+            return res.json({
+                erro: false,
+                produtos
+            });
+        }).catch(() => {
+            return res.status(400).json({
+                erro: true,
+                mensagem: "Erro: Nenhum produto encontrado!"
+            });
+        });
+});
+
+app.get("/view-produto/:id", async (req, res) => {
+    const { id } = req.params;
+    await Produto.findByPk(id)
+        .then((produto) => {
+            return res.json({
+                erro: false,
+                produto
+            });
+        }).catch(() => {
+            return res.status(400).json({
+                erro: true,
+                mensagem: "Erro: Nenhum produto encontrado!"
+            });
+        });
+});
+
+app.post("/cad-produto", async (req, res) => {
+    await Produto.create(req.body)
+        .then(() => {
+            return res.json({
+                erro: false,
+                mensagem: "Produto cadastrado com sucesso!"
+            });
+        }).catch(() => {
+            return res.status(400).json({
+                erro: true,
+                mensagem: "Erro: Produto não cadastrado com sucesso!"
+            });
+        });
+});
+
+app.put('/edit-produto', async (req, res) => {
+    const {id} = req.body;
+    await Produto.update(req.body, {where: {id}})
+    .then(() => {
+        return res.json({
+            erro: false,
+            mensagem: "Produto editado com sucesso!"
+        });
+    }).catch(() => {
+        return res.status(400).json({
+            erro: true,
+            mensagem: "Erro: Produto não editado com sucesso!"
+        });
+    });
+});
+
+app.delete('/delete-produto/:id', async (req, res) => {
+    const {id} = req.params;
+
+    await Produto.destroy({where: {id}})
+    .then(() => {
+        return res.json({
+            erro: false,
+            mensagem: "Produto apagado com sucesso!"
+        });
+    }).catch(() => {
+        return res.status(400).json({
+            erro: true,
+            mensagem: "Erro: Produto não apagado com sucesso!"
+        });
+    });
+});
+
+app.listen(8080, () => {
+    console.log("Servidor iniciado na porta 8080: http://localhost:8080");
+});
